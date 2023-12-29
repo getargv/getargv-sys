4fa3f27c56397834c72bc11b5391099c2741e9e0
Camden Narzt <c.narzt@me.com>
2023-12-29 13:41:07 -0700

    update wrapper

--- wrapper.h
+++ wrapper.h
@@ -9,17 +9,21 @@
 extern "C" {
 #endif
 
+#include <stdio.h>
 #include <sys/types.h>
+
+#ifndef __cplusplus
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
   #include <stdbool.h>
 #else
 typedef enum { false, true } bool;
 #endif
+#endif
 
 struct GetArgvOptions {
-  uint  skip;
-  pid_t pid;
-  bool  nuls;
+  uint      skip;
+  pid_t     pid;
+  bool      nuls;
 };
 struct ArgvArgcResult {
   char*  buffer;
@@ -31,6 +35,10 @@ struct ArgvResult {
   char* start_pointer;
   char* end_pointer;
 };
+
+int32_t get_arg_exact(pid_t pid);
+bool print_argv_of_pid_to(const char* start_pointer, const char* end_pointer, FILE* outstream);
+bool get_argv_of_pid_no_malloc(const struct GetArgvOptions* options, struct ArgvResult* retVal, size_t argsize);
 
 bool print_argv_of_pid(const char* start_pointer, const char* end_pointer);
 bool get_argv_of_pid(const struct GetArgvOptions* options, struct ArgvResult* result);
