2970ddf1e1c1097cb5e90600190c4b3e601b4360
Camden Narzt <c.narzt@me.com>
2025-07-28 14:07:54 -0600

    update wrapper.h

--- wrapper.h
+++ wrapper.h
@@ -14,7 +14,9 @@ extern "C" {
 
 #ifndef __cplusplus
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
+#if (__STDC_VERSION__ < 202311L)
   #include <stdbool.h>
+#endif
 #else
 typedef enum { false, true } bool;
 #endif
@@ -30,19 +32,28 @@ struct ArgvArgcResult {
   char** argv;
   uint   argc;
 };
+struct ArgvEnvpResult {
+  char*  buffer;
+  char** argv;
+  uint   argc;
+  char** envp;
+  uint   envc;
+};
 struct ArgvResult {
   char* buffer;
   char* start_pointer;
   char* end_pointer;
 };
 
-int32_t get_arg_exact(pid_t pid);
+int32_t get_arg_exact_size(pid_t pid);
 bool print_argv_of_pid_to(const char* start_pointer, const char* end_pointer, FILE* outstream);
 bool get_argv_of_pid_no_malloc(const struct GetArgvOptions* options, struct ArgvResult* retVal, size_t argsize);
 
 bool print_argv_of_pid(const char* start_pointer, const char* end_pointer);
 bool get_argv_of_pid(const struct GetArgvOptions* options, struct ArgvResult* result);
 bool get_argv_and_argc_of_pid(pid_t pid, struct ArgvArgcResult* result);
+bool get_argv_and_envp_of_pid(pid_t pid, struct ArgvEnvpResult* result);
+void free_ArgvEnvpResult(struct ArgvEnvpResult* result);
 void free_ArgvArgcResult(struct ArgvArgcResult* result);
 void free_ArgvResult(struct ArgvResult* result);
 
